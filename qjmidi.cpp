8c08e4ca11d6a239cc8708dcbe367de2b23e1314
Juanjo Martín <juanjomartin97@hotmail.es>
2019-06-13 19:01:43 +0200

    Initial Qt project files

---
+++ qjmidi.cpp
@@ -0,0 +1,14 @@
+#include "qjmidi.h"
+#include "ui_qjmidi.h"
+
+QJmidi::QJmidi(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::QJmidi)
+{
+    ui->setupUi(this);
+}
+
+QJmidi::~QJmidi()
+{
+    delete ui;
+}
